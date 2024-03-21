fc5550192e601de3c7a799ce8960eb76d12cfc70
zari <zaritsu030907@gmail.com>
2024-03-21 16:48:42 +0800

    finish lab0 byte_stream (passed) but with so confused

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -17,24 +17,26 @@ void Writer::push( string data )
     return;
   }
 
-  uint64_t len = data.length();
-  for (uint64_t i = 0; i < len; i ++) {
-    if (buffer_.size() < capacity_) {
-      pushcnt_ ++;
-      buffer_.push_back(data[i]);
-    } else {
-      set_error();
-    }
-  }
+  // uint64_t len = data.length();
+  // for (uint64_t i = 0; i < len; i ++) {
+  //   if (buffer_.size() < capacity_) {
+  //     pushcnt_ ++;
+  //     buffer_.push_back(data[i]);
+  //   } else {
+  //     set_error();
+  //   }
+  // }
 
-  return;
+  uint64_t push_size = std::min(data.length(), Writer::available_capacity());
+  // buffer_.append(data.substr(0, push_size));
+  buffer_ += data.substr(0, push_size);
+  pushcnt_ += push_size;
 }
 
 void Writer::close()
 {
   // Your code here.
   is_close_ = true;
-  return;
 }
 
 uint64_t Writer::available_capacity() const
@@ -52,7 +54,7 @@ uint64_t Writer::bytes_pushed() const
 bool Reader::is_finished() const
 {
   // Your code here.
-  return (is_close_ && buffer_.empty() && pushcnt_ == popcnt_);
+  return (is_close_  /* && buffer_.empty() */ && pushcnt_ == popcnt_);
 }
 
 uint64_t Reader::bytes_popped() const
@@ -64,24 +66,30 @@ uint64_t Reader::bytes_popped() const
 string_view Reader::peek() const
 {
   // Your code here.
-  return (buffer_.substr(0, 1));
+  if (buffer_.empty()) {
+    return {};
+  }
+  
+  return (std::string_view(buffer_));
 }
 
 void Reader::pop( uint64_t len )
 {
   // Your code here.
-  if (is_close_) {
-    return;
-  }
+  // if (is_close_) {
+  //   return;
+  // }
 
-  for (uint64_t i = 0; i < len; i ++) {
-    if (buffer_.empty()) {
-      set_error();
-    }
-    buffer_.erase(0);
-  }
+  // for (uint64_t i = 0; i < len; i ++) {
+  //   if (buffer_.empty()) {
+  //     set_error();
+  //   }
+  //   buffer_.erase(0);
+  // }
 
-  return;
+  uint64_t pop_size = std::min(len, buffer_.size());
+  buffer_ = buffer_.substr(pop_size);
+  popcnt_ += pop_size;
 }
 
 uint64_t Reader::bytes_buffered() const
